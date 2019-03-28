97d25ca0bc65dd7303cf300e4cdcb48f735519b5
Jonah <jonahkarew@gmail.com>
2019-03-28 18:10:16 -0400

    initial coding

---
+++ assets/js/game.js
@@ -0,0 +1,129 @@
+// //create all variables that contain game logic
+//   //wins (number)
+//   //losses (number)
+//   //guessLeft (number)
+//   //lettersAlreadyGuessed [array]
+//   //computerChoice
+//   //computerChoicesBank [array of alphabet]
+
+
+//VARIABLECOMPUTERCHOICES
+var computerChoicesBank = "abcdefghijklmnopqrstuvwxyz".split("");
+//VARIABLES
+var wins = 0;
+var losses = 0;
+var guessesLeft = 10;
+var userGuess ;
+var computerGuess
+var lettersAlreadyGuessed = [];
+
+//   //var computerchoicesbank = "abcdefghijklmopqrstuvwxyz".split("");
+//       //creates array of 26
+
+
+
+
+// //create variables that reference parts of page
+//   //$wins (documentgetElementById("wins"))
+//   //$losses
+//   //guessesLeft
+//   //usesrGuess
+//   //computerGuess
+//   //lettersAlreadyGuessed
+
+//VARIABLEREFERENCES
+var $wins = 0;
+var $losses = 0;
+var $guessesLeft = 10;
+var $userGuess = [];
+var $computerGuess
+var $lettersAlreadyGuessed = [];
+
+
+console.log (computerGuess)
+
+
+// //start a new game
+//   // //document.onkkeyup = function(event){
+//   //   //run logic to check guess
+//   // var compguess = compchoice[mathfloor(mathrando() *computer choices.length] ***new letter every key****
+//   // }
+
+//       //create function to start/rest game
+//         //function newgame(){
+//           reset game variables
+//           // assign compter guess new random letter
+//          reset letters guessed array
+//           lettersalreadyguessed.length = 0;
+            // reset number of guesse sback to 10
+            // indicate new game has started
+//         }
+// 
+
+function newGame() {
+        guessesLeft = 10;
+        lettersAlreadyGuessed = [];
+        computerGuess = computerChoicesBank[Math.floor(Math.random() * computerChoicesBank.length)];
+        console.log(computerGuess);
+ }
+document.onkeyup = function(){
+       
+        
+
+
+
+        userGuess = event.key;
+        lettersAlreadyGuessed.push(userGuess);
+        console.log (lettersAlreadyGuessed);
+
+        if (userGuess === computerGuess){
+                        wins++;
+                        newGame()
+                }
+         if (userGuess !== computerGuess){
+                 guessesLeft--;
+                 
+                 
+         }
+
+         if (guessesLeft === 0) {
+                 losses++;
+                newGame()
+         }
+        
+
+
+
+
+
+
+
+
+        
+        
+}
+
+
+// logic to check guess document.oneyup = function(event){
+//   run logic
+//   capture user gueess iwth event.key
+// if userguess===computerguess... you window  
+//       increase wins by 1
+//       start newgame()
+//     else
+//       decrement guesses left guesses--
+
+//       if guesses===0
+//         increases losses by 1
+//         run newgame()
+// }
+        // add letter guessed to letters guessed array using .push
+
+// })
+          // display info to page
+          //   userguess
+          //   computer guess
+          //   guessesleft
+
+          // run new Game() to start game for first time
+          newGame();
